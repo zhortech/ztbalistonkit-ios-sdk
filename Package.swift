6206d8857fea1c53538f1d7b0a3d7498a61c2173
Pavel SHPAK <p.shpak@zhortech.com>
2022-12-16 22:38:26 +0200

    Version 1.0.28

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(
             name: "ZTCoreKit", 
             url: "https://github.com/zhortech/ztcorekit-ios-sdk.git", 
-            .upToNextMajor(from: "1.1.57"))
+            .upToNextMajor(from: "1.1.59"))
     ],
     targets: [
         .target(
